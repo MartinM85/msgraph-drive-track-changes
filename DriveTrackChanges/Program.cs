ab5ed790bd402dbd0187c94d29f683a65e73e9ef
Martin Machacek <machacek@edhouse.cz>
2024-07-02 13:38:46 +0200

    Initial commit

---
+++ DriveTrackChanges/Program.cs
@@ -0,0 +1,105 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Drives.Item.Items.Item.Delta;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ODataErrors;
+
+namespace DriveTrackChanges
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Track changes");
+
+            var driveId = "Me";
+            var driveItemId = "root";
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromMinutes(10));
+
+            var credential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
+            {
+                ClientId = "<client_id>",
+                TenantId = "<tenant_id>"
+            });
+
+            var graphClient = new GraphServiceClient(credential);
+            var latestTokenResponse = await graphClient.Drives[driveId].Items[driveItemId].DeltaWithToken("latest").GetAsDeltaWithTokenGetResponseAsync(cancellationToken: cts.Token);
+            var deltaLink = latestTokenResponse.OdataDeltaLink;
+            var timestamp = DateTime.UtcNow;
+            var trackingTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    // change delay if you want to track changes more frequently
+                    await Task.Delay(TimeSpan.FromMinutes(1), cts.Token);
+                    var response = await graphClient.Drives[driveId].Items[driveItemId].Delta.WithUrl(deltaLink).GetAsDeltaGetResponseAsync(rc =>
+                    {
+                        // exclude parent folders from the response
+                        rc.Headers.Add("Prefer", "deltaExcludeParent");
+                    }, cts.Token);
+
+                    var pageIterator = PageIterator<DriveItem, DeltaGetResponse>.CreatePageIterator(graphClient, response, (item) =>
+                    {
+                        // ignore root item
+                        if (item.Name == "root")
+                        {
+                            return true;
+                        }
+
+                        if (item.Deleted != null)
+                        {
+                            Console.WriteLine($"Deleted item: {item.Id}, state: {item.Deleted.State}");
+                            return true;
+                        }
+                        if (item.CreatedDateTime > timestamp)
+                        {
+                            Console.WriteLine($"New item: {item.Id}, created: {item.CreatedDateTime}, name: {item.Name}");
+                            return true;
+                        }
+
+                        Console.WriteLine($"Modified item: {item.Id}, modified: {item.LastModifiedDateTime}, name: {item.Name}");
+                        return true;
+                    },
+                    rc =>
+                    {
+                        // exclude parent folders from the response
+                        rc.Headers.Add("Prefer", "deltaExcludeParent");
+                        return rc;
+                    });
+
+                    await pageIterator.IterateAsync(cts.Token);
+                    deltaLink = pageIterator.Deltalink;
+                    timestamp = DateTime.UtcNow;
+                }
+            });
+
+            try
+            {
+                await trackingTask;
+            }
+            catch (TaskCanceledException)
+            {
+                Console.WriteLine("Task was canceled.");
+            }
+            catch(ODataError error)
+            {
+                Console.WriteLine($"Error: {error.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            try
+            {
+                trackingTask.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # msgraph-drive-track-changes
 Example how to track changes in OneDrive with the Microsoft Graph .NET SDK
+
+## Prerequisites
+
+Create a new application in the [Entra admin portal](https://entra.cmd.ms) and grant delegated permission `Files.Read` to track changes in personal OneDrive.
+
